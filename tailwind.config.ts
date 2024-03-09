0e65797f1e51809af8b3d28ffc23fc0a2cf11484
Natan <natan-moreira1@hotmail.com>
2024-03-09 18:32:25 -0300

    style: config theme variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,40 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-vertical": `linear-gradient(180deg, #7FD1CC 0%, #9694F5 100%)`,
+        "gradient-horizontal": `linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%)`,
+      },
+      fontFamily: {
+        nsans: ["var(--font-nsans)"],
+      },
+      colors: {
+        green: {
+          100: "#50B2C0",
+          200: "#255D6A",
+          300: "#0A313C",
+        },
+        purple: {
+          100: "#8381D9",
+          200: "#2A2879",
+        },
+        gray: {
+          100: "#F8F9FC",
+          200: "#E6E8F2",
+          300: "#D1D6E4",
+          400: "#8D95AF",
+          500: "#303F73",
+          600: "#252D4A",
+          700: "#181C2A",
+          800: "#0E1116",
+        },
+      },
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.875rem",
+        md: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
       },
     },
   },
