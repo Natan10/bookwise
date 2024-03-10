88aa9da8aa8c5a06e996036a24dba5d84d54636d
Natan <natan-moreira1@hotmail.com>
2024-03-10 10:21:58 -0300

    feat: create big book card

---
+++ src/components/card/big-book-card.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Image from "next/image";
+import livro from "@/assets/books/entendendo-algoritmos.png";
+import { Stars } from "../stars";
+
+export function BigBookCard() {
+  return (
+    <div className="w-[600px] h-[192px] rounded-lg bg-gray-600">
+      <div className="px-6 py-5 grid grid-cols-[auto_1fr_1fr] gap-6">
+        <div className="rounded">
+          <Image
+            src={livro}
+            alt="livro"
+            width={108}
+            height={152}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="col-span-2">
+          <div className="flex justify-between items-center mb-3">
+            <span className="text-xs text-gray-300">Hoje</span>
+            <Stars rate={2} />
+          </div>
+          <div className="mb-[30px]">
+            <p className="text-gray-100 text-base font-bold">
+              Entendendo Algoritmos
+            </p>
+            <p className="text-sm text-gray-400">Aditya Bhargava</p>
+          </div>
+          <p className="text-gray-300 text-sm truncate">
+            Nec tempor nunc in egestas. Euismod nisi eleifend at et in sagittis.
+            Penatibus id vestibulum imperdiet a at imperdiet lectu ssssssss
+            sssssssss ssssestibulum imperdiet a at imperdiet lectu ssssssss
+            sssssssss ssssestibulum imperdiet a at imperdiet lectu ssssssss
+            sssssssss ssss
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/stars.tsx
@@ -0,0 +1,25 @@
+import { Star } from "@phosphor-icons/react";
+
+export function Stars({ rate }: { rate: number }) {
+  const stars = new Array(5);
+
+  for (let i = 0; i < 5; i++) {
+    if (i <= rate - 1) {
+      stars[i] = 1;
+    } else {
+      stars[i] = 0;
+    }
+  }
+
+  return (
+    <div className="flex items-center gap-[5px]">
+      {stars.map((star, i) =>
+        star === 1 ? (
+          <Star key={i} size={14} weight={"fill"} className="text-purple-100" />
+        ) : (
+          <Star key={i} size={14} color="#8381D9" />
+        )
+      )}
+    </div>
+  );
+}
