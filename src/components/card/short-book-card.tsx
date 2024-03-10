577603eb19c99519ab27936e7fe09e53cd9be24b
Natan <natan-moreira1@hotmail.com>
2024-03-10 10:26:49 -0300

    feat: create short book card

---
+++ src/components/card/short-book-card.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Image from "next/image";
+import livro from "@/assets/books/fragmentos-do-horror.png";
+import { Stars } from "../stars";
+
+export function ShortBookCard() {
+  return (
+    <div className="w-[308px] h-[130px] rounded-lg bg-gray-600">
+      <div className="px-5 py-[18px] grid grid-cols-[auto_1fr_1fr] gap-6">
+        <div className="rounded">
+          <Image
+            src={livro}
+            alt="livro"
+            width={64}
+            height={94}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="col-span-2">
+          <div className="mb-[34px]">
+            <p className="text-gray-100 text-base font-bold">
+              Fragmentos de Horror
+            </p>
+            <p className="text-sm text-gray-400">Junji Ito</p>
+          </div>
+          <Stars rate={2} />
+        </div>
+      </div>
+    </div>
+  );
+}
