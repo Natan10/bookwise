f68f0c14885750edf4483e030c7815006c1c89a4
Natan <natan-moreira1@hotmail.com>
2024-03-10 11:11:31 -0300

    feat: create tag component

---
+++ src/components/tag.tsx
@@ -0,0 +1,28 @@
+type TagProps = {
+  title: string;
+  isSelected?: boolean;
+};
+
+export function Tag({ title, isSelected = false }: TagProps) {
+  return (
+    <label
+      htmlFor="type"
+      className={`
+      group px-4 py-1
+      rounded-full border border-purple-100 hover:bg-purple-200
+      text-sm text-purple-100 hover:text-gray-100
+      has-[input:checked]:bg-purple-200 has-[input:checked]:text-gray-100 has-[input:checked]:border-purple-200
+      cursor-pointer
+    `}
+    >
+      {title}
+      <input
+        id="type"
+        type="checkbox"
+        checked={isSelected}
+        className="hidden"
+        value={title}
+      />
+    </label>
+  );
+}
