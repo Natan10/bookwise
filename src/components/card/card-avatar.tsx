c94138c72a8bec83d103c44844dc442bf6ef737c
Natan <natan-moreira1@hotmail.com>
2024-03-10 17:43:07 -0300

    feat: create card-avatar

---
+++ src/components/card/card-avatar.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Image from "next/image";
+import { Stars } from "../stars";
+import * as Avatar from "@/components/avatar";
+
+export function CardAvatar() {
+  return (
+    <div className="w-[608px] h-[280px] p-6 rounded-lg bg-gray-600 transition-all">
+      <div className="flex justify-between items-start">
+        <Avatar.AvatarRoot>
+          <Avatar.AvatarPhoto
+            avatarUrl="https://github.com/natan10.png"
+            profileName="Natan"
+          />
+          <Avatar.AvatarDescription name="Natan" description="Hoje" />
+        </Avatar.AvatarRoot>
+        <Stars rate={2} />
+      </div>
+
+      <div className="mt-8 grid grid-cols-[auto_1fr_1fr] gap-5">
+        <div className="rounded">
+          <Image
+            src={"/images/books/o-guia-do-mochileiro-das-galaxias.png"}
+            alt="livro"
+            width={108}
+            height={152}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="col-span-2">
+          <div className="mb-5">
+            <p className="text-gray-100 text-base font-bold">
+              Entendendo Algoritmos
+            </p>
+            <p className="text-sm text-gray-400">Aditya Bhargava</p>
+          </div>
+          <div className="h-20 flex ">
+            <p className="text-gray-300 text-sm text-ellipsis overflow-hidden">
+              Nec tempor nunc in egestas. Euismod nisi eleifend at et in
+              sagittis. Penatibus id vestibulum imperdiet a at imperdiet lectu
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
