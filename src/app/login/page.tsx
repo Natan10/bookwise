3de6e3625e5c5ad85cc69b79f317ebe267742e24
Natan moreira <natan-moreira1@hotmail.com>
2024-05-16 21:17:05 -0300

    feat: adjust login page style

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,28 +9,34 @@ export default function Login() {
   return (
     <main className="h-full bg-gray-800">
       <section className="h-full p-5 grid grid-cols-2">
-        <div className="w-full relative">
-          <Image src={login} alt="login" fill />
+        <div className="relative rounded-[10px] overflow-hidden">
+          <Image src={login} alt="login" fill style={{ objectFit: "cover" }} />
         </div>
         <div className="flex flex-col justify-center items-center">
           <div className="mb-10 w-[372px] flex flex-col justify-start">
-            <h1 className="font-bold text-xl/8">Boas vindas!</h1>
-            <h2 className="text-sm/10">
+            <h1 className="font-bold text-xl/8 text-gray-100">Boas vindas!</h1>
+            <h2 className="text-sm/10 text-gray-200">
               Faça seu login ou acesse como visitante.
             </h2>
           </div>
           <div className="w-[372px] flex flex-col gap-5">
             <button className="flex items-center gap-5 p-5 bg-gray-600 rounded-lg hover:bg-gray-500 transition-colors outline-none">
               <Image src={google} alt="google" width={30} height={30} />
-              <span className="text-md font-bold">Entrar com o Google</span>
+              <span className="text-md font-bold text-gray-200">
+                Entrar com o Google
+              </span>
             </button>
             <button className="flex items-center gap-5 p-5 bg-gray-600 rounded-lg hover:bg-gray-500 transition-colors outline-none">
               <Image src={github} alt="github" width={30} height={30} />
-              <span className="text-md font-bold">Entrar com o Github</span>
+              <span className="text-md font-bold text-gray-200">
+                Entrar com o Github
+              </span>
             </button>
             <button className="flex items-center gap-5 p-5 bg-gray-600 rounded-lg hover:bg-gray-500 transition-colors outline-none">
               <Image src={rocket} alt="rocket" width={30} height={30} />
-              <span className="text-md font-bold">Acessar como visitante</span>
+              <span className="text-md font-bold text-gray-200">
+                Acessar como visitante
+              </span>
             </button>
           </div>
         </div>
