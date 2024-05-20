c8d60a32f90ebbd7c9503e3cc9f61ad16124ed54
Natan moreira <natan-moreira1@hotmail.com>
2024-05-20 14:19:35 +0200

    feat: add query context

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from "react";
 import { NavigationSidebar } from "@/components/navigation-sidebar";
+import { QueryProvider } from "@/contexts/query-context";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="h-full flex bg-gray-800">
-      <NavigationSidebar />
-      <section className="flex-1 overflow-y-auto">{children}</section>
-    </main>
+    <QueryProvider>
+      <main className="h-full flex bg-gray-800">
+        <NavigationSidebar />
+        <section className="flex-1 overflow-y-auto">{children}</section>
+      </main>
+    </QueryProvider>
   );
 }
