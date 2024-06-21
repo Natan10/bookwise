40aaaf3b1ac33855fc7563a87f83367734f50944
Natan moreira <natan-moreira1@hotmail.com>
2024-06-21 09:46:23 -0300

    feat: adjust drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,9 +7,5 @@ export default {
   out: "./src/infra/database/migrations",
   dbCredentials: {
     url: process.env.CONNECTION_STRING!,
-    // host: process.env.POSTGRES_HOST!,
-    // port: +process.env.POSTGRES_PORT!,
-    // password: process.env.POSTGRES_PASSWORD!,
-    // database: process.env.POSTGRES_DATABASE!,
   },
 } satisfies Config;
